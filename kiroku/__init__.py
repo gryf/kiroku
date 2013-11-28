fe4e1f47d0a2b611c124aa72cc730c67d1bad447
Roman Dobosz <gryf73@gmail.com>
2013-11-28 21:47:28 +0100

    Removed description for deleted pygmrnts module

--- kiroku/__init__.py
+++ kiroku/__init__.py
@@ -3,7 +3,6 @@ This is a kiroku module, which contains modules as follows:
     __init__.py - this module
     kiroku.py - main kiroku module
     naive_tzinfo.py - naive implementation of tzinfo class
-    pygments_directive.py - custom pygments directive for docutils
     rest.py - custom classes for generating articles out of reST source
     search.py - helper for indexing words in articles.
 """
